7ffaddc9c9fb568eda7a0a07ea1381db4ebe4361
Gerson Calienes <gersoncalienes@gmail.com>
2017-08-08 18:28:24 -0400

    finished now to work on the fanciness

--- index.js
+++ index.js
@@ -6,19 +6,25 @@ var RESULTS = document.getElementById("js-search-results");
 BTN.addEventListener("click", function() {
 	var LAT = document.getElementById("latitude").value;
 	var LONG = document.getElementById('longitude').value;
-	console.log(LAT,LONG);
+	//console.log(LAT,LONG);
 	event.preventDefault();
-	var ourRequest = new XMLHttpRequest();
-	ourRequest.open('GET', 'https://api.sunrise-sunset.org/json?lat='+ LAT+ '&lng='+LONG);
-	var ourData = JSON.parse(ourRequest.responseText);
-	renderHTML(ourData);
-	ourRequest.send();
-	console.log(ourRequest.responseText);
+	
+	$.ajax({
+		url: 'https://api.sunrise-sunset.org/json?lat='+ LAT+ '&lng='+LONG,
+		dataType: "text",
+		success:function(data) {
+			
+			var json = $.parseJSON(data);
+			$('#js-search-results').html('The sunset will occur at ' + json.results.sunset);
+		}
+	});
+
+	//tried to just use js but it didn't work
+	//var ourRequest = new XMLHttpRequest();
+	//ourRequest.open('GET', 'https://api.sunrise-sunset.org/json?lat='+ LAT+ '&lng='+LONG);
+	//var ourData = JSON.parse(ourRequest.responseText);
+	//renderHTML(ourData);
+	//ourRequest.send();
+	//console.log(ourRequest.responseText);
 });
 
-function renderHTML(data){
-	
-
-	RESULTS.innerHTML= data;
-
-};
