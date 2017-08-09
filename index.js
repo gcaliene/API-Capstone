005d4e1e62c5bc840b460fead09ef95f01f05ec6
Gerson Calienes <gersoncalienes@gmail.com>
2017-08-08 20:02:20 -0400

    added geolocation

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 var BTN = document.getElementById("Submit");
 var RESULTS = document.getElementById("js-search-results");
 
+//this part of the code obtains the current geolocation of the individual 
+var x = document.getElementById('current');
+function getLocation(){
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(showPosition);
+	} else {
+		x.innerHTML = "Geolocation is not supported by this browser.";
+	}
+}
 
-
+//This part will display in the HTML the current Latitude and Longitude
+function showPosition (position) {
+	x.innerHTML = "Latitude: " + position.coords.latitude +
+	"<br>Longitude: " +position.coords.longitude;
+}
 BTN.addEventListener("click", function() {
 	var LAT = document.getElementById("latitude").value;
 	var LONG = document.getElementById('longitude').value;
