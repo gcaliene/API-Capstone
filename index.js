26f9873a5c1207f4a0020392325cecd13d8a6412
Gerson Calienes <gersoncalienes@gmail.com>
2017-08-14 10:35:13 -0400

    final before final touches. fully functional

--- index.js
+++ index.js
@@ -3,41 +3,113 @@ var RESULTS = document.getElementById("js-search-results");
 
 //this part of the code obtains the current geolocation of the individual 
 var x = document.getElementById('current');
+
+//gets the location from the user's device.
 function getLocation(){
 	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(showPosition);
+		navigator.geolocation.getCurrentPosition(showPosition, showError);
+		//$(".js-search-form").addClass("hidden"); not necessary
+
 	} else {
+		console.log("user denied");
 		x.innerHTML = "Geolocation is not supported by this browser.";
+		$(".js-search-form").removeClass("hidden");
 	}
 }
 
+
+//On home page there will be the small heading and the button to begin.
+$("#begin").click(function () {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(showPosition, showError);
+		$(".centralHeading").addClass("hidden");
+		//$(".js-search-form").addClass("hidden"); not necessary
+
+
+	} else {
+		console.log("user denied");
+		x.innerHTML = "Geolocation is not supported by this browser.";
+		$(".js-search-form").removeClass("hidden");
+	}	// body...
+});
+
+//in case of errors.
+function showError(error) {
+	console.log(error);
+    switch(error.code) {
+        		case error.PERMISSION_DENIED:
+           			x.innerHTML = "User denied the request for Geolocation. <br> Please refresh and allow request for Geolocation"
+           			//$(".js-search-form").removeClass("hidden");
+            		break;
+        		case error.POSITION_UNAVAILABLE:
+           			x.innerHTML = "Location information is unavailable."
+           			//$(".js-search-form").removeClass("hidden");
+            		break;
+        		case error.TIMEOUT:
+            		x.innerHTML = "The request to get user location timed out."
+            		//$(".js-search-form").removeClass("hidden");
+            		break;
+        		case error.UNKNOWN_ERROR:
+            		x.innerHTML = "An unknown error occurred."
+            		break;
+    		}
+		}	
+
+
 //This part will display in the HTML the current Latitude and Longitude
-function showPosition (position) {
-	x.innerHTML = "Latitude: " + position.coords.latitude +
-	"<br>Longitude: " +position.coords.longitude;
-}
-BTN.addEventListener("click", function() {
-	var LAT = document.getElementById("latitude").value;
-	var LONG = document.getElementById('longitude').value;
-	//console.log(LAT,LONG);
-	event.preventDefault();
+function showPosition (position, error) {
+	x.innerHTML = "<h2>Results</h2>" + "Your geographic coordinate system is:"+"<br>Latitude: " + position.coords.latitude +
+	"<br>Longitude: " + position.coords.longitude;
 	
+	$("#Submit").click(function() { 
+			var LAT = document.getElementById("latitude").value;
+			var LONG = document.getElementById('longitude').value;
+	
+			//event.preventDefault();
+			$.ajax({
+				url: 'https://api.sunrise-sunset.org/json?lat='+ LAT+ '&lng='+LONG ,
+				dataType: "text",
+				success:function(data) {
+					var json = $.parseJSON(data);
+					$('#js-search-results').html('The sunrise will occur at <br> <' +toLocalTime(json.results.sunrise));
+				}
+			});
+		});
+
+//This returns the time of the sunrise
 	$.ajax({
-		url: 'https://api.sunrise-sunset.org/json?lat='+ LAT+ '&lng='+LONG,
+		url: 'https://api.sunrise-sunset.org/json?lat='+ position.coords.latitude + '&lng='+ position.coords.longitude +'&date=today',
 		dataType: "text",
 		success:function(data) {
-			
 			var json = $.parseJSON(data);
-			$('#js-search-results').html('The sunset will occur at ' + json.results.sunset);
+			$('#js-search-results').html('The sunrise will occur at <br> ' + toLocalTime(json.results.sunrise));
 		}
 	});
+}
 
-	//tried to just use js but it didn't work
-	//var ourRequest = new XMLHttpRequest();
-	//ourRequest.open('GET', 'https://api.sunrise-sunset.org/json?lat='+ LAT+ '&lng='+LONG);
-	//var ourData = JSON.parse(ourRequest.responseText);
-	//renderHTML(ourData);
-	//ourRequest.send();
-	//console.log(ourRequest.responseText);
-});
+//The next thing I would have to do is the conversion of the time to the local time. 
+function toLocalTime(UTCDateString){
+		console.log(UTCDateString);
+        var CLT = new moment.utc(UTCDateString, 'hh:mm:ss a');
+        console.log(CLT);
+        var newDate = CLT.clone().local().format("h:mm:ss A"); 
+       // var newDate = new Date(CLT.getTime()+convertedLocalTime.getTimezoneOffset()*60*1000);
+        console.log(newDate);
+   // var offset = date.getTimezoneOffset() / 60;
+  //  var hours = date.getHours();
+
+  //  newDate.setHours(hours - offset);
+
+    return newDate; 
+       // console.log(Date(convertedLocalTime + 'UTC').toString());
+     //   console.log()
+     //   var hourOffset = convertedLocalTime.getTimezoneOffset() / 60;
+
+      //  convertedLocalTime.setHours(convertedLocalTime.getHours() + hourOffset ); 
+      //  return convertedLocalTime;
+}
+
+
+
+
 
