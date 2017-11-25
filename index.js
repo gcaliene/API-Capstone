1f0d5bae3fa3c73475c4e7c3faf5513140a14bd7
Unknown <gersoncalienes@gmail.com>
2017-11-24 19:57:30 -0500

    cleaned up callback

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+//https://www.w3schools.com/html/html5_geolocation.asp for reference
 var BTN = document.getElementById("Submit");
 var RESULTS = document.getElementById("js-search-results");
 var x = document.getElementById('current');
 
 
 
-//this part of the code obtains the current geolocation of the individual 
+//this part of the code obtains the current geolocation of the individual
 function getLocation(){
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(showPosition, showError);
@@ -15,22 +16,38 @@ function getLocation(){
 	}
 }
 
+//On home page there will be the small heading and the button to begin the callback.
+$("#begin").click(getLocation);
 
 
-//On home page there will be the small heading and the button to begin.
-$("#begin").click(function () {
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(showPosition, showError);
-		$(".centralHeading").addClass("hidden");
-	} else {
-		console.log("user denied");
-		x.innerHTML = "Geolocation is not supported by this browser.";
-		$(".js-search-form").removeClass("hidden");
-	}	
-});
 
 
-//in case of errors.
+
+//This part will display in the HTML the current Latitude and Longitude as well as make the AJAX call to the API and returns the time of the sunrise.
+function showPosition (position, error) { //
+	console.log(position);
+	x.innerHTML = "<h2>Results</h2>" + "Your geographic coordinate system is:"+ "<br>Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+	$.ajax({
+		url: 'https://api.sunrise-sunset.org/json?lat='+ position.coords.latitude + '&lng='+ position.coords.longitude +'&date=today',
+		dataType: "text",
+		success:function(data) {
+			var json = $.parseJSON(data);
+			$('#js-search-results').addClass("sunrise");
+			$('#js-search-results').html('The sunrise will occur at <br> ' + toLocalTime(json.results.sunrise));
+		}
+	});
+}
+
+//The next thing I would have to do is the conversion of the time to the local time.
+function toLocalTime(UTCDateString){
+    var CLT = new moment.utc(UTCDateString, 'hh:mm:ss a');
+    var newDate = CLT.clone().local().format("h:mm:ss A");
+    console.log(newDate);
+	return newDate;
+}
+
+
+//ERROR Handler
 function showError(error) {
     switch(error.code) {
         case error.PERMISSION_DENIED:
@@ -46,31 +63,4 @@ function showError(error) {
             x.innerHTML = "An unknown error occurred."
             break;
     }
-}	
-
-
-//This part will display in the HTML the current Latitude and Longitude as well as make the AJAX call to the API and returns the time of the sunrise.
-function showPosition (position, error) {
-	x.innerHTML = "<h2>Results</h2>" + "Your geographic coordinate system is:"+ "<br>Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
-	$.ajax({
-		url: 'https://api.sunrise-sunset.org/json?lat='+ position.coords.latitude + '&lng='+ position.coords.longitude +'&date=today',
-		dataType: "text",
-		success:function(data) {
-			var json = $.parseJSON(data);
-			$('#js-search-results').addClass("sunrise");
-			$('#js-search-results').html('The sunrise will occur at <br> ' + toLocalTime(json.results.sunrise));
-		}
-	});
 }
-
-//The next thing I would have to do is the conversion of the time to the local time. 
-function toLocalTime(UTCDateString){
-    var CLT = new moment.utc(UTCDateString, 'hh:mm:ss a');
-    var newDate = CLT.clone().local().format("h:mm:ss A"); 
-    console.log(newDate);
-	return newDate; 
-}
-
-
-
-
